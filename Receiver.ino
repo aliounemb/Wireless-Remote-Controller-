69580b571707f9e8f37c07094ae9767ad1389b83
El Hadji Aliou Mbaye <aliounemb95@gmail.com>
2017-05-18 14:55:44 -0400

    Update Receiver.ino

--- Receiver.ino
+++ Receiver.ino
@@ -24,36 +24,17 @@ short RF24csnPin = 10;
 
 short motorPin = 7;
 
-short redPin = 3;
-short greenPin = 5;
-short bluePin = 6;
 
 #define MOTOR 0
 #define HORIZONTAL 1
-#define CBUTTON 3
 #define ZBUTTON 2
 
-//Leds
-int stillColor = STILLCOLOR;
-int colorChangeSpeed = COLORCHANGESPEED;
-
-int newBlue = 0;
-int newRed = 0;
-int newGreen = 0;
-
-int blueValue = 0;
-int greenValue = 0;
-int redValue = 0;
-
-short color = 0;
-
-//Initialisations
 
 bool hold = false;
 bool discoMode = false;
 bool crazyColor = false;
-bool beginnerMode = false;
-bool brakeAllowed = true;
+bool beginnerMode = false; // false
+bool brakeAllowed = true;   
 
 bool lastCButtonState = false;
 bool lastZButtonState = false;
@@ -64,112 +45,11 @@ long lastColorChange;
 long lastColorDisco;
 long lastAcceleration;
 
-int msg[4];
+int msg[3];
 
 Servo motor;
 RF24 radio(RF24cePin, RF24csnPin);
 const uint64_t pipe = 0xE8E8F0F0E1LL;
-
-void setColor() {
-  switch (color) {
-    case 0:
-      newBlue = 255;
-      newGreen = 0;
-      newRed = 0;
-      break;
-    case 1:
-      newBlue = 0;
-      newGreen = 255;
-      newRed = 0;
-      break;
-    case 2:
-      newBlue = 0;
-      newGreen = 0;
-      newRed = 255;
-      break;
-    case 3:
-      newBlue = 255;
-      newGreen = 255;
-      newRed = 0;
-      break;
-    case 4:
-      newBlue = 0;
-      newGreen = 255;
-      newRed = 255;
-      break;
-    case 5:
-      newBlue = 255;
-      newGreen = 0;
-      newRed = 255;
-      break;
-    case 6:
-      newBlue = 255;
-      newGreen = 255;
-      newRed = 255;
-      break;
-    case 7:
-      newBlue = 0;
-      newGreen = 0;
-      newRed = 0;
-      break;
-  }
-}
-
-void refreshLed() {
-  if (millis() - lastColorChange > colorChangeSpeed) {
-    lastColorChange = millis();
-    if (blueValue > newBlue) {
-      blueValue--;
-    } else if ( blueValue < newBlue) {
-      blueValue++;
-    }
-    if (greenValue > newGreen) {
-      greenValue--;
-    } else if (greenValue < newGreen) {
-      greenValue++;
-    }
-    if (redValue > newRed) {
-      redValue--;
-    } else if (redValue < newRed) {
-      redValue++;
-    }
-  }
-
-  if (discoMode && (millis() - lastColorDisco) > stillColor) {
-    lastColorDisco = millis();
-    if (color >= 6) color = 0;
-    else color++;
-    setColor();
-  }
-
-  analogWrite(bluePin, blueValue);
-  analogWrite(greenPin, greenValue);
-  analogWrite(redPin, redValue);
-}
-
-void ledButtonPressed() {
-  if (discoMode && !crazyColor) { //Turn led off
-    discoMode = false;
-    color = 7;
-    setColor();
-    redValue = 0;
-    blueValue = 0;
-    greenValue = 0;
-    analogWrite(bluePin, blueValue);
-    analogWrite(greenPin, greenValue);
-    analogWrite(redPin, redValue);
-  } else {
-    discoMode = true;
-    crazyColor = !crazyColor;
-    if (crazyColor) {
-      stillColor = STILLCOLORCRAZY;
-      colorChangeSpeed = 0;
-    } else {
-      stillColor = STILLCOLOR;
-      colorChangeSpeed = COLORCHANGESPEED;
-    }
-  }
-}
 
 void sendMotor(int value) {
   if (value >= 89 && !beginnerMode) motor.write(value);
@@ -185,9 +65,9 @@ void getData() {
     brakeAllowed = false;
   }
   if (radio.available()) {
-    while (radio.available()) radio.read(msg, 7); //Get data
+    while (radio.available()) radio.read(msg, sizeof(msg)); //Get data
     if (msg[MOTOR] >= 89) brakeAllowed = true;
-    if (!msg[CBUTTON] && lastCButtonState) ledButtonPressed();
+    //if (!msg[CBUTTON] && lastCButtonState) ledButtonPressed();
     if (msg[ZBUTTON] && !beginnerMode) sendMotor(msg[MOTOR]);
     else if (msg[ZBUTTON] && beginnerMode) {
       if (msg[MOTOR] >= 89) sendMotor(map(msg[MOTOR], 0, 180, (90 - 90 / BEGINNERDIV), (90 + 90 / BEGINNERDIV)));
@@ -215,7 +95,7 @@ void getData() {
     else if (msg[ZBUTTON] && msg[HORIZONTAL] <= 5 && lastHorizontalValue > 5) Serial.write(1);
 
     lastZButtonState = msg[ZBUTTON];
-    lastCButtonState = msg[CBUTTON];
+    //lastCButtonState = msg[CBUTTON];
     lastHorizontalValue = msg[HORIZONTAL];
     lastRead = millis();
   }
@@ -230,18 +110,17 @@ void setup() {
   radio.openReadingPipe(1, pipe);
   radio.startListening();
 
-  pinMode(redPin, OUTPUT);
-  pinMode(greenPin, OUTPUT);
-  pinMode(bluePin, OUTPUT);
-
   lastRead = millis();
-  lastColorChange = millis();
-  lastColorDisco = millis();
   lastAcceleration = millis();
 }
 
+int treatValue(int data) {
+  return (data * 9 / 1024) + 48;
+ }
+ 
 void loop() {
   getData();
-  refreshLed();
+ // Serial.print("vertical");
+ // Serial.println(treatValue(lastHorizontalValue));
 
 }
